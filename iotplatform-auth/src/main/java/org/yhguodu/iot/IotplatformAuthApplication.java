ea672e6cc86c0f7fdb2d0af918c01da1a86b8789
黄泽 <yonghnguodu@hotmail.com>
2017-05-15 17:55:24 +0800

    add iot project

---
+++ iotplatform-auth/src/main/java/org/yhguodu/iot/IotplatformAuthApplication.java
@@ -0,0 +1,12 @@
+package org.yhguodu.iot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotplatformAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotplatformAuthApplication.class, args);
+	}
+}
