2ace8583795d66a80d921031c48cbff4553a3323
黄泽 <yonghnguodu@hotmail.com>
2017-10-12 17:46:08 +0800

    修正jackson.databind 错误

--- iot-datacenter/src/main/java/org/yhguodu/iot/IotDatacenterApplication.java
+++ iot-datacenter/src/main/java/org/yhguodu/iot/IotDatacenterApplication.java
@@ -2,10 +2,10 @@ package org.yhguodu.iot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class IotDatacenterApplication {
 
 	public static void main(String[] args) {
--- iot-portal/src/main/java/org/yhguodu/iot/IotPortalApplication.java
+++ iot-portal/src/main/java/org/yhguodu/iot/IotPortalApplication.java
@@ -2,7 +2,7 @@ package org.yhguodu.iot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
 public class IotPortalApplication {
